91d1cf62b4ed878cf53c0c0151aacbf4c39e274f
getjoey <43150950+getjoey@users.noreply.github.com>
2019-02-04 15:46:48 -0500

    update
    added variable search and extra parsing
    and made it oop

--- kijiji.py
+++ kijiji.py
@@ -3,13 +3,9 @@ import requests
 from urllib.request import urlopen
 import time
 
-#SEARCH FOR SOMETHING AND PUT THE URL YOU WANT HERE.
-#This is an example for search (PS4).
-url = 'https://www.kijiji.ca/b-ville-de-montreal/ps4/k0l1700281?dc=true'
-
 def getUrls(url):
 	page_content = urlopen(url)
-	time.sleep(3)
+	time.sleep(1)
 	soup = BeautifulSoup(page_content.read(), "html.parser")
 	#get next page link
 	next_pages = soup.find_all("span", class_="page-link")
@@ -23,7 +19,7 @@ def getUrls(url):
 
 def getItems(url):
 	page_content = urlopen(url)
-	time.sleep(3)
+	time.sleep(1)
 	#data = data.read()
 
 	soup = BeautifulSoup(page_content.read(), "html.parser")
@@ -46,8 +42,10 @@ def getItems(url):
 	for m, n in zip(range(len(titles)), range(len(prices))):
 		try:
 			tuple = [2]
-			tuple[0] = titles[m].a.string.strip()
-			tuple.append(prices[n].string.strip().replace("\xa0$",""))
+			tuple[0] = titles[m].a.string.strip().lower()
+			p = prices[n].string.strip().replace("\xa0$","")
+			p = p.replace(",",".")
+			tuple.append(p)
 			#tuple = {titles[m].a.string.strip(), prices[n].string.strip().replace("\xa0$","")}  
 			#.strip removes the stupid whitespace and \n
 			#the .replace is to remove the weird "\xa0$"
@@ -56,23 +54,117 @@ def getItems(url):
 			#print(prices[n].string)	
 		except:
 			pass
-			
-	for a in items:
-		print(a)
+	
+	return items
 
+def parseFor(kItems,keyWords, priceMin, priceMax):
+	#items is our tuple [name, price]
+	#We will parse for our keyWord, and range of price to show only!
+	
+	goodItems = []
+	
+	for i in kItems:
+		try:
+			found = 0 #ensures if multiple key words in string it doesnt add it multiple times...
+			for k in keyWords:
+				if(i[0].find(k.lower()) != -1 and found == 0):  #using lower to ensure always lower case
+					if(float(i[1]) <= priceMax and float(i[1])>= priceMin):
+						goodItems.append(i)
+						found = 1
+			found = 0
+		except:
+			pass
+	
+	return goodItems
 
-#main code
-kLinks = getUrls(url)
-print(kLinks)
-
-for i in range(len(kLinks)):	
+def runIt(url, keyWords, priceRange):
 	print()
-	print("i = ",i)
-	getItems(kLinks[i])
-	print("done ",i)
+	print("Starting Search on......")
+	print("Url: ",url)
+	print("Key Words : ",keyWords)
+	print("PriceRange: ",priceRange)
+	print()
+	allItems = []
+	goodItems = []
+	
+	#Step 1 get first 7 pages of searched linked
+	kLinks = getUrls(url)
+	print("Got some pages")
 	print()
 
+	#step 2 Get all items on those pages in a tuple[name,price]
+	for i in kLinks:
+		x = getItems(i)
+		allItems.append(x)
+	print("Got all items")
+	print()
 
+	#step 3 parse all items for our keywords, and price range
+	for i in range(len(allItems)):
+		goodItems.append(parseFor(allItems[i],  keyWords, priceRange[0], priceRange[1]))
+	print("Parsed items for keyWords")
+	time.sleep(1)
+	print()
+		
+	#step 4 print them out nicely to show hits and page#
+	print("RESULTS: ")
+	for m in range(len(goodItems)):
+		print("Page #",m)
+		for n in range(len(goodItems[m])):
+			print(n," ",goodItems[m][n])
+	
+	print()
+	print("Done")
 	
 
-print("done")
+class KijijiParser:
+	def __init__(self,url,keyWords,priceRange):
+		self.url = url
+		self.keyWords = keyWords
+		self.priceRange = priceRange
+		
+	def runParser(self):
+		url = self.url
+		keyWords = self.keyWords
+		priceRange = self.priceRange
+		print()
+		print("Starting Search on......")
+		print("Url: ",url)
+		print("Key Words : ",keyWords)
+		print("PriceRange: ",priceRange)
+		print()
+		allItems = []
+		goodItems = []
+		
+		#Step 1 get first 7 pages of searched linked
+		kLinks = getUrls(url)
+		print("Got some pages")
+		print()
+
+		#step 2 Get all items on those pages in a tuple[name,price]
+		for i in kLinks:
+			x = getItems(i)
+			allItems.append(x)
+		print("Got all items")
+		print()
+
+		#step 3 parse all items for our keywords, and price range
+		for i in range(len(allItems)):
+			goodItems.append(parseFor(allItems[i],  keyWords, priceRange[0], priceRange[1]))
+		print("Parsed items for keyWords")
+		time.sleep(1)
+		print()
+			
+		#step 4 print them out nicely to show hits and page#
+		print("RESULTS: ")
+		for m in range(len(goodItems)):
+			print("Page #",m)
+			for n in range(len(goodItems[m])):
+				print(n," ",goodItems[m][n])
+		
+		print()
+		print("Done")
+
+
+		
+
---
+++ main.py
@@ -0,0 +1,78 @@
+import kijiji
+from kijiji import KijijiParser
+
+#SEARCH FOR SOMETHING AND PUT THE URL YOU WANT HERE.
+#This is an example for search (PS4).
+url = 'https://www.kijiji.ca/b-ville-de-montreal/ps4/k0l1700281?dc=true'
+
+#HERE WE PUT keyWords and price range
+keyWords = ["ps4","playstation", "4", "station", "console"]  #as long as 1 key word shows up itll be a hit!
+priceRange = [150,500] 
+
+#Create the object
+k = KijijiParser(url,keyWords,priceRange)
+k.runParser() #Run the search
+
+Print("done")
+
+#Here is some sample output 
+#------------------------
+#------------------------
+
+"""
+Starting Search on......
+Url:  https://www.kijiji.ca/b-ville-de-montreal/ps4/k0l1700281?dc=true
+Key Words :  ['ps4', 'playstation', '4', 'station', 'console']
+PriceRange:  [150, 500]
+
+Done Scanning for Kijiji Page Links
+Got some pages
+
+Got all items
+
+Parsed items for keyWords
+
+RESULTS:
+Page # 0
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 slim à vendre', '400.00']
+2   ['ps4 1tb spider-man bundle (neuve) facture pour garantie', '380.00']
+3   ['ps4 pro comme neuve!', '450.00']
+4   ['ps4 pro 1500 gb', '450.00']
+5   ['ps4 slim 1tb', '350.00']
+Page # 1
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['écouteur avec micro headphone astro a40 neuf pour ps4 et autre', '250.00']
+Page # 2
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 pro 1to neuf scellé', '500.00']
+2   ['sony playstation 4', '360.00']
+3   ['playstation 4 pro - 1tb - like new', '400.00']
+4   ['recherché :billets de kiss à échanger contre ps4', '280.00']
+Page # 3
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 noir de jais 1to', '330.00']
+2   ['jeux de ps4', '250.00']
+Page # 4
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 pro brand new avec battlefied 5', '460.00']
+2   ['ps 4 - 1tb slim - spider-man bundle', '395.00']
+Page # 5
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 pro', '500.00']
+2   ['console ps4 slim 1tb avec spider-man et red dead redemption 2', '350.00']
+3   ['ps4', '300.00']
+4   ['ps4 for sale.', '250.00']
+Page # 6
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['playstation 4 + 2 manettes +1 jeu', '350.00']
+Page # 7
+0   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+1   ['ps4 a vendre', '280.00']
+2   ['recherché :ps4 dans la boîte a vendre', '350.00']
+3   ['console ps4 comme neuve / playstation 4 console like new', '280.00']
+4   ['ps4 500gb + 5 games and tv', '400.00']
+5   ['ps4 slim brand new for 440$ negotiable with fifa 2018 & nhl 2018', '440.00']
+
+Done
+"""
